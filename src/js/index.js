ac01fa94705d7cffdcebc126746fc79e09eca539
Dmitry <dvoskoboev@gmail.com>
2021-10-24 00:18:56 +0300

    Animated bubbles in brand slide, formatted scss.

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,15 @@ initHeroSwiper()
 function initHeroSwiper() {
   const slider = document.querySelector('.swiper')
 
-  // Swiper.use([Navigation])
-
   // eslint-disable-next-line no-unused-vars
   const swiper = new Swiper(slider, {
     // modules: [],
     speed: 400,
-    initialSlide: 2,
+    initialSlide: 0,
     slidesPerView: 1,
     loop: false,
     slideClass: 'swiper-slide',
     navigation: {
-      // nextEl: '.swiper-button-next',
       nextEl: '.main__btn',
       // prevEl: '.swiper-button-prev'
     },
@@ -30,10 +27,8 @@ initBrandSwiper()
 function initBrandSwiper() {
   const slider1 = document.querySelector('.swiper--brand')
 
-  // Swiper.use([Naviga
   // eslint-disable-next-line no-unused-vars
   const swiper1 = new Swiper(slider1, {
-    // modules: [],
     speed: 400,
     initialSlide: 0,
     slidesPerView: 1,
@@ -52,60 +47,56 @@ function initBrandSwiper() {
       crossFade: true
     }
   })
-
-
 }
 
-// moveToFirstSlide()
+moveToFirstSlide()
 
 function moveToFirstSlide() {
-  const wrapper = document.querySelector('.swiper-wrapper')
-  const homeBtn = document.querySelector('.header__nav-link')
-  const allSlides = document.querySelectorAll('.swiper-slide')
-  const firstSlide = document.querySelector('.swiper-slide--1')
-  const secondSlide = document.querySelector('.swiper-slide--2')
+  const wrapper = document.querySelector('.swiper-wrapper'),
+    homeBtn = document.querySelector('.header__nav-img'),
+    allSlides = document.querySelectorAll('.swiper-slide'),
+    firstSlide = document.querySelector('.swiper-slide--1'),
+    secondSlide = document.querySelector('.swiper-slide--2')
 
-  const slideActivationClass = 'swiper-slide-active'
+  const slideActivationClass = 'swiper-slide-active' // ?
 
-  homeBtn.addEventListener('click', ev => {
-    ev.preventDefault()
-
+  const clearAncillaryClasses = () => {
     allSlides.forEach(item => {
       item.classList.remove(slideActivationClass)
       item.classList.remove('swiper-slide-prev')
       item.classList.remove('swiper-slide-next')
+
+
     })
+  }
 
+  const addRelevantClassesAndCSSValues = () => {
     firstSlide.classList.add(slideActivationClass)
-    secondSlide.classList.add('swiper-slide-next')
+    // secondSlide.classList.add('swiper-slide-next')
     wrapper.style.transform = 'translate3d(0px, 0px, 0px)'
-    wrapper.style.transitionDuration = '400ms'
-  })
+    // wrapper.style.transitionDuration = '400ms'
+  }
+
+  const moveToSlide = () => {
+    clearAncillaryClasses()
+    addRelevantClassesAndCSSValues()
+  }
+
+  homeBtn.addEventListener('click', moveToSlide)
 }
 
-// moveDecorationElemsOnLoad()
 
-function moveDecorationElemsOnLoad() {
+// moveToSecondSlide()
 
-  const bacteriumOne = document.querySelector('.main__bacterium--1')
-  const bacteriumTwo = document.querySelector('.main__bacterium--2')
-  const bacteriumThree = document.querySelector('.main__bacterium--3')
-  const bacteriumFour = document.querySelector('.main__bacterium--4')
-  const bacteriumFive = document.querySelector('.main__bacterium--5')
-  // const bacteriumSix = document.querySelector('.main__bacterium--6')
-  // const bacteriumSeven = document.querySelector('.main__bacterium--7')
+// function moveToSecondSlide() {
+//   wrapper = document.querySelector('.swiper-wrapper')
+//   firstSlide = document.querySelector('.swiper-slide--1'),
+//   secondSlide = document.querySelector('.swiper-slide--2')
+
+// }
 
 
-  window.addEventListener('load', () => {
-    bacteriumOne.classList.add('bacterium__one--active')
-    bacteriumTwo.classList.add('bacterium__two--active')
-    bacteriumThree.classList.add('bacterium__three--active')
-    bacteriumFour.classList.add('bacterium__four--active')
-    bacteriumFive.classList.add('bacterium__five--active')
-  })
 
-  const moveElems = () => { }
-}
 
 // moveBacteria()
 
@@ -155,20 +146,23 @@ function moveBacteria() {
 
 }
 
-// enableScroll()
+enableDescriptionScroll()
 
-function enableScroll() {
+function enableDescriptionScroll() {
+  const input = document.querySelector('.descr__scroll-input'),
+    block = document.querySelector('.descr__text-container-inner'),
+    blockHeightInPixels = document.querySelector(
+      '.descr__text-container-inner'
+    ).clientHeight,
+    percentHeight = blockHeightInPixels / 100,
+    scrollThumbMitigationCoefficient = 2.1 // Set by visual experience
 
-  const input = document.querySelector('.descr__scroll-input')
-  const block = document.querySelector('.descr__text-container-inner')
+  const setValueToScroll = () => {
+    block.scrollTop =
+      (input.value * percentHeight) / scrollThumbMitigationCoefficient
+  }
 
-  const blockHeight = document.querySelector('.descr__text-container-inner').clientHeight
-
-  const percentHeight = blockHeight / 100
-
-  input.addEventListener('input', () => {
-    block.scrollTop = input.value * percentHeight / 2.1
-  })
+  input.addEventListener('input', setValueToScroll)
 }
 
 toggleModal()
@@ -185,9 +179,9 @@ function toggleModal() {
 
   const closeModal = () => {
     modal.classList.remove(activationClass)
+
   }
 
   openBtn.addEventListener('click', openModal)
   closeBtn.addEventListener('click', closeModal)
 }
-
